47efe11781e309e6292e4e2a8a5cd5adbfdd2fc5
vvvsrx <vvvsrx@gmail.com>
2017-03-16 20:41:42 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WebSocketNoticeServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发消息(Type:正在配送中)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "发消息(Type:最近订单)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(28, 158);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "启动";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(109, 158);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "发消息(Type:完成陪送)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 199);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "消息测试";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SuperSocket.SocketBase;
+using SuperSocket.WebSocket;
+using Newtonsoft.Json;
+
+namespace WebSocketNoticeServer
+{
+    public partial class Form1 : Form
+    {
+        WebSocketServer wsServer = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.btnStart.BackColor = Color.Green;
+            this.btnStop.BackColor = Color.Red;
+            this.btnStop.Enabled = false;
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+
+            wsServer = new WebSocketServer();
+
+            if (!wsServer.Setup(2017))
+            {
+                MessageBox.Show("Failed to setup!");
+                Application.Exit();
+            }
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var session in wsServer.GetAllSessions())
+            {
+                session.Send(JsonConvert.SerializeObject(new MessageModel { Command = 2, Data = new { Text = "您有1个订单正在配送中" } }));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var session in wsServer.GetAllSessions())
+            {
+                session.Send(JsonConvert.SerializeObject(new MessageModel { Command = 2, Data = new { Text = "您最近有999个订单" } }));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (var session in wsServer.GetAllSessions())
+            {
+                session.Send(JsonConvert.SerializeObject(new MessageModel { Command = 2, Data = new { Text = "订单已收货" } }));
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            this.btnStart.BackColor = Color.Green;
+            this.btnStop.BackColor = Color.Red;
+            this.btnStop.Enabled = false;
+            this.btnStart.Enabled = true;
+            this.button1.Enabled = true;
+            this.button2.Enabled = true;
+            this.button3.Enabled = true;
+            wsServer.Stop();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!wsServer.Start())
+            {
+                MessageBox.Show("Failed to start!");
+                return;
+            }
+            this.btnStart.BackColor = Color.Red;
+            this.btnStop.BackColor =  Color.Green;
+            this.btnStop.Enabled = true;
+            this.btnStart.Enabled = false;
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+        }
+    }
+
+    public class MessageModel
+    {
+        public int Command { get; set; }
+
+        public object Data { get; set; }
+    }
+}
